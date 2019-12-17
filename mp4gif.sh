0e4da181625bf17a4102565246053cfcfbcbdc18
Takeru Kobayashi <31469598+takeru1990@users.noreply.github.com>
2019-12-17 16:48:20 +1100

    Create mp4gif.sh
    スクリプトの本体

---
+++ mp4gif.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+(ffmpeg -i ${1} -vf "palettegen" -y ~/palette.png)  2>/dev/null
+(ffmpeg -i ${1} -i ~/palette.png -lavfi "fps=12,scale=900:-1:flags=lanczos [x]; [x][1:v] paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" -y ${1}.gif)  2>/dev/null
+rm ~/palette.png
